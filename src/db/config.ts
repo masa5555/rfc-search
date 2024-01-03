ae223e7a39c9b48b86ad6c904a4a6068ea4e4828
masa5555 <masa57887@gmail.com>
2024-01-04 00:28:59 +0900

    Fix: Vercelで環境変数が読み込めない問題

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,15 +1,13 @@
+import process from 'node:process';
 import {drizzle} from 'drizzle-orm/planetscale-serverless';
 import {connect} from '@planetscale/database';
-import {configDotenv} from 'dotenv';
 import * as schema from './schema/index.ts';
-
-const result = configDotenv({path: './.env.local'});
 
 // Create the connection
 const connection = connect({
-  host: result.parsed?.DATABASE_HOST ?? '',
-  username: result.parsed?.DATABASE_USERNAME ?? '',
-  password: result.parsed?.DATABASE_PASSWORD ?? '',
+  host: process.env.DATABASE_HOST ?? '',
+  username: process.env.DATABASE_USERNAME ?? '',
+  password: process.env.DATABASE_PASSWORD ?? '',
 });
 
 export const db = drizzle(connection, {schema});
